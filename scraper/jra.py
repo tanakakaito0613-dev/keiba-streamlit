1e87509bc5d89579775e993b657b1fe2636fe5fa
tanakakaito0613-dev <tanakakaito0613@gmail.com>
2026-02-05 22:21:24 +0900

    Update jra.py

--- scraper/jra.py
+++ scraper/jra.py
@@ -80,7 +80,7 @@ def get_race_info(url):
         res = requests.get(url, headers=headers, timeout=10)
         print("status:", res.status_code)
         print("length:", len(res.text))
-        print(res.text[:500])
+        print(res.text[:1000])
         res.raise_for_status()
     except requests.exceptions.RequestException as e:
         print("HTTP error:", e)
@@ -157,6 +157,7 @@ def get_race_info(url):
         horses.append(horse)
 
     return horses
+
 
 
 
